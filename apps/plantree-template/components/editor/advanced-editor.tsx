212125bfda81ede33f2ce4ece0f8ed1617c40efe
0xzio <zio.penx@gmail.com>
2024-10-19 11:00:20 +0800

    feat: disable redis

--- apps/plantree-template/components/editor/advanced-editor.tsx
+++ apps/plantree-template/components/editor/advanced-editor.tsx
@@ -44,7 +44,7 @@ const Editor = ({
   return (
     <EditorRoot>
       <EditorContent
-        immediatelyRender={false}
+        // immediatelyRender={false}
         className={cn('editor h-full', className)}
         editable={editable}
         {...(initialValue && { initialContent: initialValue })}
